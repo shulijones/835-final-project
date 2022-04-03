a05061bfc779f6ac97f909b146e902127eaaba84
Emily Caragay <caragay@mit.edu>
2022-04-03 19:42:18 -0400

    basic implementation for detecting a frame held to the camera

---
+++ frame_finding.py
@@ -0,0 +1,17 @@
+from cmath import inf
+import cv2
+import numpy as np
+img = cv2.imread("less_easy.jpg", -1)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret,thresh_img = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
+thresh_img = cv2.dilate(thresh_img, np.ones((45, 45), np.uint8)) 
+thresh_img = cv2.medianBlur(thresh_img, 25)
+
+contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+contours = sorted(contours, key=cv2.contourArea)
+
+x,y,w,h = cv2.boundingRect(contours[-2])
+cropped_image = img[y:y+h,x :x+w]
+cv2.imshow("main", cropped_image)
+cv2.waitKey(0)
