e5ed4a1c87ce48166f2ca9587d6c3b6fdaf87f03
Emily Caragay <caragay@mit.edu>
2022-04-18 19:12:08 -0400

    Remove wall identification step, split picture location step into two

--- frame_finding.py
+++ frame_finding.py
@@ -1,7 +1,7 @@
 from cmath import inf
 import cv2
 import numpy as np
-img = cv2.imread("less_easy.jpg", -1)
+img = cv2.imread("held_to_wall.jpg", -1)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 ret,thresh_img = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
@@ -11,7 +11,8 @@ thresh_img = cv2.medianBlur(thresh_img, 25)
 contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 contours = sorted(contours, key=cv2.contourArea)
 
-x,y,w,h = cv2.boundingRect(contours[-2])
+x,y,w,h = cv2.boundingRect(contours[-3])
 cropped_image = img[y:y+h,x :x+w]
+cv2.namedWindow('main', cv2.WINDOW_KEEPRATIO)
 cv2.imshow("main", cropped_image)
 cv2.waitKey(0)
